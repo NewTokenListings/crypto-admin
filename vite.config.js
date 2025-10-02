dd4fdd0a28b83766756658f6c6189b3bd91a6021
NewTokenListings <newtokenlistingsmail@gmail.com>
2025-10-02 10:42:42 +0000

    fix(build): lock vite base to root for custom domain admin.newtokenlistings.com

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // If deploying to a custom domain, base should be "/"
-  base: "/",
+  base: "/", // 👈 Always build for custom domain
 });
