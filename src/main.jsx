79409bfa67075e004b6780d4d7cc90d5a0eeacf4
NewTokenListings <newtokenlistingsmail@gmail.com>
2025-10-02 15:06:56 +0000

    Update main.jsx for Hello World test / ErrorBoundary wrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import ErrorBoundary from "./components/ErrorBoundary.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <h1>Hello World</h1>
   </React.StrictMode>
 );
